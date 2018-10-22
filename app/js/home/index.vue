392a603985e891b96c47268d50e46717e8c3cb46
xujianshe <1556300171@qq.com>
2018-10-22 10:30:01 +0800

    10.23

---
+++ app/js/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="$style.home">
+        <Header></Header>
+        <Slider></Slider>
+    </div>
+</template>
+
+<script>
+import Header from '../public/header.vue'
+import Slider from './hslider.vue'
+export default {
+  components: {
+    Header,
+    Slider
+  }
+}
+</script>
+
+<style lang="scss" module>
+  @import '../../css/reset.scss';
+  .home{
+    color: red;
+    font-size: 80px;
+  }
+</style>
